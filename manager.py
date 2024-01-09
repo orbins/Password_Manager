c91d50e7464cbb68c9db701dfcdccdd9fdee184b
William <orwellj@mail.ru>
2024-01-09 23:57:44 +0300

    changing add_password func

--- manager.py
+++ manager.py
@@ -1,3 +1,4 @@
+from getpass import getpass
 import logging
 from pathlib import Path
 import sqlite3
@@ -13,11 +14,40 @@ logger.addHandler(stream_handler)
 
 class PasswordManager:
 
-    def check_db_file(self):
-        ...
+    def __init__(self):
+        self.db_file = Path(__file__).cwd() / "passwords.db"
 
     def add_password(self):
-        ...
+        try:
+            connection = sqlite3.connect("passwords.db")
+        except ConnectionError:
+            logger.error('Файл базы данных не найден, не удалось установить соединение!')
+            return
+        cursor = connection.cursor()
+        table = cursor.execute(
+            """
+            SELECT name FROM sqlite_master WHERE type='table' AND name='passwords'
+            """
+        ).fetchone()
+        if table:
+            service_name = input("Введите имя сервиса: ")
+            login = input('Введите логин: ')
+            is_additional_info = input('Наличие доп. данных? Введите y: ')
+            if is_additional_info.lower() == 'y':
+                info = input('Введите доп. инфу: ')
+            is_accepted = input(f"Имя сервис: {service_name}\nЛогин: {login}\nДоп. данные: {info}. Подтвердить y/n: ")
+            if is_accepted.lower() == 'y':
+                password = getpass("Введите пароль: ")
+                password2 = getpass("Повторите пароль: ")
+                if password == password2:
+                    ...
+                else:
+                    logging.error("Пароли не похожи, попробуйте заново!")
+                    self.add_password()
+            else:
+                self.add_password()
+        else:
+            logging.error('В файле БД отсутствует таблица с паролями!')
 
     def select_service(self):
         ...
@@ -26,15 +56,17 @@ class PasswordManager:
         ...
 
     def main(self):
-        self.check_db_file()
-        action = input(
-            "Выберите действие:" 
-            "\n1.Add\n2.Copy\n3.Change\n4.Generate"
-        )
-        match action.lower():
-            case 'add': self.add_password()
-            case 'copy', 'change': self.select_service()
-            case 'generate': self.generate_password()
+        if self.db_file.exists():
+            action = input(
+                "Выберите действие: " 
+                "\n1.Add\n2.Copy\n3.Change\n4.Generate"
+            )
+            match action.lower():
+                case 'add': self.add_password()
+                case 'copy', 'change': self.select_service()
+                case 'generate': self.generate_password()
+        else:
+            logging.error('Не удалось найти файл базы данных!')
 
 
 if __name__ == '__main__':
