682394b15bec31bbdc304be72056c6ed0de9b342
William <orwellj@mail.ru>
2024-01-09 23:04:13 +0300

    add logging

--- manager.py
+++ manager.py
@@ -1,4 +1,20 @@
+import logging
+from pathlib import Path
+import sqlite3
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+
 class PasswordManager:
+
+    def check_db_file(self):
+        ...
 
     def add_password(self):
         ...
@@ -10,14 +26,14 @@ class PasswordManager:
         ...
 
     def main(self):
+        self.check_db_file()
         action = input(
             "Выберите действие:" 
             "\n1.Add\n2.Copy\n3.Change\n4.Generate"
         )
         match action.lower():
             case 'add': self.add_password()
-            case 'copy': self.select_service()
-            case 'change': self.select_service()
+            case 'copy', 'change': self.select_service()
             case 'generate': self.generate_password()
 
 
